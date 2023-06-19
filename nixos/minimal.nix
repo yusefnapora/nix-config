68379f0b89f789b73974e0d9719b9d1a1eecd3ff
Yusef Napora <yusef@napora.org>
2023-06-19 23:20:37 +0000

    add hyprland binary cache config

--- nixos/minimal.nix
+++ nixos/minimal.nix
@@ -40,6 +40,10 @@
       experimental-features = "nix-command flakes";
       # Deduplicate and optimize nix store
       auto-optimise-store = true;
+
+      # add binary cache for hyprland
+      substituters = ["https://hyprland.cachix.org"];
+      trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
     };
   };
 
