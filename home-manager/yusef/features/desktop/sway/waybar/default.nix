5faae9eed3d592c483cac6eaf234c45a70b25d6c
Yusef Napora <yusef@napora.org>
2023-09-13 17:32:39 +0000

    waybar: use custom clock module
    built in one breaks on vmware for reasons unknown

--- home-manager/yusef/features/desktop/sway/waybar/default.nix
+++ home-manager/yusef/features/desktop/sway/waybar/default.nix
@@ -15,11 +15,21 @@ in {
         height = 32;
         modules-left = [ "sway/workspaces" "sway/mode" "wlr/taskbar" ];
         modules-center = [ "sway/window" ];
-        modules-right = [ "tray" "clock" "pulseaudio" "battery" ];
+        modules-right = [ "tray" "custom/clock" "pulseaudio" "battery" ];
 
         "sway/workspaces" = {
           disable-scroll = true;
           all-outputs = true;
+        };
+
+        "custom/clock" = {
+          return-type = "json";
+          exec =  ''
+            text=$(date +'%I:%M %p')
+            tt=$(date +'%A, %B %d, %Y')
+            echo "{\"text\": \"$text\", \"tooltip\": \"$tt\", \"class\": \"custom-clock\"}"
+          '';
+          interval = 5;
         };
 
         clock = {
