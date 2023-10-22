cb9b4756f2602c6e43f71c7fe25205917884461b
Yusef Napora <yusef@napora.org>
2023-10-22 17:48:33 -0400

    firefox: change new tab page to kagi

--- home-manager/yusef/features/desktop/common/firefox.nix
+++ home-manager/yusef/features/desktop/common/firefox.nix
@@ -22,7 +22,7 @@ in {
           "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
           "browser.shell.checkDefaultBrowser" = false;
           "browser.shell.defaultBrowserCheckCount" = 1;
-          "browser.startup.homepage" = "https://start.duckduckgo.com";
+          "browser.startup.homepage" = "https://kagi.com";
           "privacy.trackingprotection.enabled" = true;
           "browser.uiCustomization.state" = ''{"placements":{"widget-overflow-fixed-list":[],"unified-extensions-area":["ublock0_raymondhill_net-browser-action"],"nav-bar":["back-button","forward-button","stop-reload-button","customizableui-special-spring1","urlbar-container","customizableui-special-spring2","downloads-button","fxa-toolbar-menu-button","_d634138d-c276-4fc8-924b-40a0ea21d284_-browser-action"],"toolbar-menubar":["menubar-items"],"TabsToolbar":["firefox-view-button","tabbrowser-tabs","new-tab-button","alltabs-button"],"PersonalToolbar":["import-button","personal-bookmarks"]},"seen":["save-to-pocket-button","developer-button","_d634138d-c276-4fc8-924b-40a0ea21d284_-browser-action","ublock0_raymondhill_net-browser-action"],"dirtyAreaCache":["nav-bar","PersonalToolbar","toolbar-menubar","TabsToolbar","unified-extensions-area"],"currentVersion":18,"newElementCount":2}'';
         };
