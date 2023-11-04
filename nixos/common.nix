56090a83d15b86957250f3d466c5e67f2c92a9ff
Yusef Napora <yusef@napora.org>
2023-11-04 14:42:46 -0400

    enable udisks service in common config

--- nixos/common.nix
+++ nixos/common.nix
@@ -8,7 +8,8 @@
     ./features/nix-ld-vscode.nix
   ];
 
-  
+  # enable mounting external media without root privs
+  services.udisks2.enable = true;
 
   # The rest of the configuration is set by each host config, which will
   # import this file and extend to suit each host.
