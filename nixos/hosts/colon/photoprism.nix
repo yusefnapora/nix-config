585accafeb2b0c6ef14f4b52630dc8972c3763eb
Yusef Napora <yusef@napora.org>
2024-01-01 14:40:02 -0500

    accept letsencrypt terms

--- nixos/hosts/colon/photoprism.nix
+++ nixos/hosts/colon/photoprism.nix
@@ -37,6 +37,11 @@
     } ];
   };
 
+  security.acme = {
+    acceptTerms = true;
+    defaults.email = "yusef@napora.org";
+  };
+
   services.nginx = {
     enable = true;
     recommendedTlsSettings = true;
