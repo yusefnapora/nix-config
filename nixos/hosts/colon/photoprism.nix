8034491c8d164cc72566c03377bbb5e5877dd884
Yusef Napora <yusef@napora.org>
2024-01-01 14:49:41 -0500

    hopefully fix nginx config

--- nixos/hosts/colon/photoprism.nix
+++ nixos/hosts/colon/photoprism.nix
@@ -61,7 +61,6 @@
             proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
             proxy_set_header Host $host;
             proxy_buffering off;
-            proxy_http_version 1.1;
           '';
         };
       };
