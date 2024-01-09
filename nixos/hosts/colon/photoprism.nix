a04f58982d082301f8f30f2300e860dfa634860d
Yusef Napora <yusef@napora.org>
2024-01-09 21:54:36 +0000

    photoprism: fix proxy config

--- nixos/hosts/colon/photoprism.nix
+++ nixos/hosts/colon/photoprism.nix
@@ -57,11 +57,6 @@
         locations."/" = {
           proxyPass = "http://127.0.0.1:2342";
           proxyWebsockets = true;
-          extraConfig = ''
-            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-            proxy_set_header Host $host;
-            proxy_buffering off;
-          '';
         };
       };
     };
