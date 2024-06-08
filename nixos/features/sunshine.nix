1f5c9e80f438e1706a28c914e2fef7db01fccaf7
Yusef Napora <yusef@napora.org>
2024-06-08 20:20:15 +0000

    sunshine: use module from nixpkgs

--- nixos/features/sunshine.nix
+++ nixos/features/sunshine.nix
@@ -1,44 +1,11 @@
 # adapted from https://github.com/bphenriques/dotfiles/blob/master/nixos/modules/services/sunshine.nix
 { config, lib, pkgs, ... }:
-let sunshine-pkg = pkgs.sunshine.override { cudaSupport = true; stdenv = pkgs.cudaPackages.backendStdenv; };
-in
-with lib;
 {
 
-  # From https://docs.lizardbyte.dev/projects/sunshine/en/latest/about/usage.html#linux
-  # https://docs.lizardbyte.dev/projects/sunshine/en/latest/about/advanced_usage.html#port
-  networking.firewall = {
-    allowedTCPPorts = [ 47984 47989 47990 48010 ];
-    allowedUDPPorts = [ 47998 47999 48000 48002 ];
-  };
-
-  # Make it work for KMS.
-  security.wrappers.sunshine = {
-    owner = "root";
-    group = "root";
-    capabilities = "cap_sys_admin+p";
-    source = "${sunshine-pkg}/bin/sunshine";
-  };
-
-  # Requires to simulate input
-  boot.kernelModules = [ "uinput" ];
-  services.udev.extraRules = ''
-    KERNEL=="uinput", SUBSYSTEM=="misc", OPTIONS+="static_node=uinput", TAG+="uaccess"
-  '';
-
-  environment.systemPackages = [ sunshine-pkg ];
-  #systemd.packages = with pkgs; [ sunshine ];
-
-  systemd.user.services.sunshine-temp-v5 = {
+  services.sunshine = {
     enable = true;
-    description = "Starts Sunshine";
-    wantedBy = ["graphical-session.target"];
-    startLimitIntervalSec = 500;
-    startLimitBurst = 5;
-    serviceConfig = {
-       Restart = "on-failure";
-       RestartSec = 5;
-       ExecStart = "${sunshine-pkg}/bin/sunshine";
-     };
+    package = pkgs.sunshine.override { cudaSupport = true; };
+    capSysAdmin = true;
+    openFirewall = true;
   };
 }
